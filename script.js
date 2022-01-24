db70a1ce075760a248e79437d61f6906080bd7a8
andrei-z <andrei.zhulin@gmail.com>
2022-01-24 15:42:53 +0100

    rock-scissors-paper-console-version

---
+++ script.js
@@ -0,0 +1,73 @@
+function computerPlay(){
+    const choices = ['rock', 'paper', 'scissors'];
+
+    let computerChoice = Math.floor(Math.random() * choices.length);
+    return choices[computerChoice];
+}
+
+function playRound(playerSelection, computerSelection){
+    const winMessage = `You win!!! ${playerSelection} beats ${computerSelection}!`;
+    const lossMessage = `You loose! ${playerSelection} is beaten by ${computerSelection} :(`;
+    let isWinner = true;
+
+    // Player picks "Rock"
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors'){
+        console.log(winMessage);
+        return isWinner;
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper'){
+        console.log(lossMessage);
+        return !isWinner;
+    }
+
+    // Player picks "Scissors"
+    if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper'){
+        console.log(winMessage);
+        return isWinner;
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock'){
+        console.log(lossMessage);
+        return !isWinner;
+    }
+
+    // Player picks "Paper"
+    if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock'){
+        console.log(winMessage);
+        return isWinner;
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors'){
+        console.log(lossMessage);
+        return !isWinner;
+    }
+
+    // Player picks same item as Computer
+    if (playerSelection.toLowerCase() === computerSelection){
+        console.log(`It's a tie!! ${playerSelection} equals ${computerSelection}`);
+        return null;
+    }
+}
+
+function game(numberOfRounds = 5){
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < numberOfRounds; i++){
+        let playerChoice = prompt(`Rock, Scissors or Paper? (attempt ${i+1}/${numberOfRounds})`);
+
+        let roundResult = playRound(playerChoice, computerPlay());
+
+        if (roundResult === true){
+            playerScore++;
+        }else if(roundResult === false){
+            computerScore++;
+        }
+    }
+
+    // Output final results
+    if(playerScore > computerScore){
+        console.log(`You Win!!! (${playerScore} vs ${computerScore})`);
+    }else if(playerScore < computerScore) {
+        console.log(`You Loose! (${playerScore} vs ${computerScore})`);
+    }else {
+        console.log(`Nobody wins! (${playerScore} vs ${computerScore})`);
+    }
+}
+
+game(3);
